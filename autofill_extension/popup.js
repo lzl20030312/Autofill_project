ff166e6f2f5b9ded6fad27e786ab0f4fd4848b04
Zile Liu <95832673+lzl20030312@users.noreply.github.com>
2026-08-25 13:19:32 -0400

    Add files via upload
    additional fields are able to be filled

---
+++ autofill_extension/popup.js
@@ -0,0 +1,19 @@
+document.getElementById("autofill").addEventListener("click", async () => {
+  const data = {
+    description: document.getElementById("description").value,
+    size: document.getElementById("size").value,
+    shelf: document.getElementById("shelf").value,
+    sku: document.getElementById("sku").value,
+    price: document.getElementById("price").value
+  };
+
+  const [tab] = await chrome.tabs.query({
+    active: true,
+    currentWindow: true
+  });
+
+  chrome.tabs.sendMessage(tab.id, {
+    action: "AUTOFILL",
+    data: data
+  });
+});
